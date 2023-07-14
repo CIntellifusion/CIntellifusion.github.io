1add4d8cb74e6573debc3d763adcb5e8720474d8
HaoyuWu556 <96377955+CIntellifusion@users.noreply.github.com>
2023-07-14 21:40:50 +0800

    Delete Lexico directory

--- Lexico/cc/build-lexicoapp-unknown-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "D:/programs/anaconda/Library/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "D:/programs/anaconda/Library" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "D:/programs/visual studio/visual studio ide/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "D:/programs/visual studio/visual studio ide/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
